d2d53d9206570e4473b4bca1a8d05d6dfd2bab3a
hocntph22459 <hocntph22459@fpt.edu.vn>
2023-02-23 10:40:24 +0700

    edit menu

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,25 @@
 const NotFound = () => {
-	return /* html */ `
-    <div class='flex items-center justify-center flex-col w-full h-screen bg-lightMode'>
-      <h1 class='text-[120px] font-bold'>404</h1>
-      <h2 class='md:text-[60px] text-[30px] capitalize text-center'>page not found!</h2>
-      <a href="/" class='py-2 px-6 rounded bg-blue-400 font-semibold capitalize mt-6 text-white'>back to portfolio</a>
-    </div>
-  `;
-};
+	return `
+  <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+	<h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+	<div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+		Page Not Found
+	</div>
+	<button class="mt-5">
+      <a
+        class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+      >
+        <span
+          class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+
+        <span class="relative block px-8 py-3 bg-[#1A2238] border border-current">
+          <router-link to="/">Go Home</router-link>
+        </span>
+      </a>
+    </button>
+</main>
+  `
+}
 
 export default NotFound;
